3cb48dc71405dfb33b925361f61d7251c5d6641e
Marissa Vargas <marissa.lorena@hotmail.com>
2023-08-04 23:05:59 -0600

    vite, react, ts

---
+++ index.tsx
@@ -0,0 +1,9 @@
+// index.tsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <h1>Hello, React with Vite and TypeScript!</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ vite.config.js
@@ -0,0 +1,6 @@
+// vite.config.js
+import react from '@vitejs/plugin-react';
+
+export default {
+  plugins: [react()],
+};
